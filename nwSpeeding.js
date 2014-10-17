db0fb0e1bc37c29dde3454c9f69cf752194e1ba9
webAddie <web.addie@gmail.com>
2014-10-16 20:14:53 -0600

    no message

--- nwSpeeding.js
+++ nwSpeeding.js
@@ -1,3 +1,21 @@
 /**
  * Created by one on 10/16/2014.
  */
+function isSpeeding(speed,options){
+    if(!options){
+        options = {};
+    }
+    if(!options.limit){
+        options.limit = 60;
+    }
+    if(options.birthday){
+        limit += 5;
+    }
+    if(speed <= options.limit){
+        return 0;
+    } else if(speed > limit && speed <= (limit+20)){
+        return 1;
+    } else {
+        return 2;
+    }
+}
--- speeding.js
+++ speeding.js
@@ -1,3 +1,14 @@
 /**
  * Created by one on 10/16/2014.
  */
+function isSpeeding(speed,isBirthday){
+    var check = speed;
+    var level = 0;
+    if (isBirthday === true) {check = speed - 5;}
+    if (check < 60) {level = 0;}
+    else if (check < 80) {level = 1;}
+    else {level = 2;}
+    return level;
+}
+
+console.log(isSpeeding(64,true));
