2c72860d2aa35be4e03e645b26f7083d2a1c2a7e
webAddie <web.addie@gmail.com>
2014-10-16 20:47:44 -0600

    no message

--- shbricks.js
+++ shbricks.js
@@ -1,3 +1,15 @@
 /**
  * Created by one on 10/16/2014.
  */
+function mason(small,large,total_length){
+    var totalSmall = small;
+    var totalLarge = large*5;
+    var availableLength = totalSmall + totalLarge;
+    if(availableLength >= total_length){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+console.log(mason(2,1,20));
