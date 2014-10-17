f634c7dcdefed8bc7b8a5d3747790ef3ee506080
webAddie <web.addie@gmail.com>
2014-10-16 20:10:27 -0600

    no message

--- longestWord.js
+++ longestWord.js
@@ -1,3 +1,16 @@
 /**
  * Created by one on 10/16/2014.
  */
+function findLongestWord(str){
+    var words = str.trim().split(" ");
+    console.log(words); //this displays the array
+    var longest = "";
+    for(var i=0; i<words.length; i++){
+        var word=words[i];
+        if (word.length>longest.length){
+            longest=word;
+        }
+    }
+    return longest;
+}
+console.log(findLongestWord("you have julie"));
